b2d63cb24a1d9ca504db3a5f1549cc87d8512713
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-04-24 23:53:28 +0900

    Initial Commit

---
+++ Sources/SSCollectionViewPresenter/SSCollectionViewPresenter.swift
@@ -0,0 +1,10 @@
+//
+//  SSCollectionViewPresenter.swift
+//  SSCollectionViewPresenter
+//
+//  Created by SunSoo Jeon on 2021/04/24.
+//
+
+import UIKit
+
+public final class SSCollectionViewPresenter: NSObject {}
---
+++ Tests/SSCollectionViewPresenterTests/SSCollectionViewPresenterTests.swift
@@ -0,0 +1,13 @@
+//
+//  SSCollectionViewPresenterTests.swift
+//  SSCollectionViewPresenter
+//
+//  Created by SunSoo Jeon on 2021/04/24.
+//
+
+import XCTest
+@testable import SSCollectionViewPresenter
+
+import UIKit
+
+class SSCollectionViewPresenterTests: XCTestCase {}
