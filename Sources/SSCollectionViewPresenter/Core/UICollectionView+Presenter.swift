b96f30b6da63695279bc365812c28ccb75ce4db9
Sunny Jeon <dSunny90@users.noreply.github.com>
2021-05-22 21:15:57 +0900

    Add scrollViewWillEndDragging-based paging snap behavior

--- Sources/SSCollectionViewPresenter/Core/UICollectionView+Presenter.swift
+++ Sources/SSCollectionViewPresenter/Core/UICollectionView+Presenter.swift
@@ -109,6 +109,30 @@ extension UICollectionView {
         return flowLayout.minimumLineSpacing
     }
 
+    /// Section inset resolved from delegate -> layout default -> .zero.
+    internal var flowLayoutSectionInset: UIEdgeInsets {
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return .zero }
+
+        let section = indexPathsForVisibleItems.sorted().first?.section ?? 0
+
+        // 1. Delegate override?
+        if let inset = (delegate as? UICollectionViewDelegateFlowLayout)?.collectionView?(self, layout: flowLayout, insetForSectionAt: section) {
+            return inset
+        }
+        // 2. Layout default
+        return flowLayout.sectionInset
+    }
+
+    /// Returns the first fractional page index.
+    internal var firstPage: CGFloat { return 0 }
+
+    /// Returns the last fractional page index.
+    internal var lastPage:CGFloat {
+        guard pageLength > 0 else { return 0 }
+
+        return (sectionLength + flowLayoutLineSpacing) / pageLength - 1
+    }
+
     internal var boundsLength: CGFloat {
         guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return 0 }
         return flowLayout.scrollDirection == .horizontal ? bounds.width : bounds.height
@@ -136,12 +160,23 @@ extension UICollectionView {
         return flowLayout.scrollDirection == .horizontal ? contentInset.right : contentInset.bottom
     }
 
+    private var sectionInsetLeading: CGFloat {
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return 0 }
+        return flowLayout.scrollDirection == .horizontal ? flowLayoutSectionInset.left : flowLayoutSectionInset.top
+    }
+
+    private var sectionInsetTrailing: CGFloat {
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return 0 }
+        return flowLayout.scrollDirection == .horizontal ? flowLayoutSectionInset.right : flowLayoutSectionInset.bottom
+    }
+
     private var itemLength: CGFloat {
         guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else { return 0 }
         return flowLayout.scrollDirection == .horizontal ? flowLayoutItemSize.width : flowLayoutItemSize.height
     }
 
     private var pageLength: CGFloat { itemLength + flowLayoutLineSpacing }
+    private var sectionLength: CGFloat { contentLength - (sectionInsetLeading + sectionInsetTrailing) }
 
     private var minOffset: CGFloat { -contentInsetLeading }
     private var maxOffset: CGFloat { contentLength - boundsLength + contentInsetTrailing }
@@ -230,6 +265,55 @@ extension UICollectionView {
             withReuseIdentifier: String(describing: UICollectionReusableView.self),
             for: indexPath
         )
+    }
+
+    /// Computes and snaps the target content offset for paging based on swipe
+    /// velocity and the current flow layout, and writes the result into
+    /// targetContentOffset.
+    ///
+    /// - Parameters:
+    ///   - velocity: Scrolling velocity in points/second. Used to bias toward
+    ///               next/previous page (ceil/floor) on a fast flick.
+    ///   - targetContentOffset: A pointer to the target content offset provided
+    ///     by UIScrollViewDelegate; this method updates its pointee to the
+    ///     computed page-aligned value.
+    internal func remapTargetContentOffset(withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        guard let scrollDirection = (collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection else { return }
+
+        let checkOffset: CGFloat = contentInsetLeading - sectionInsetLeading
+
+        let velocityValue = scrollDirection == .horizontal ? velocity.x : velocity.y
+        let targetOffset = currentOffset + velocityValue * 60.0
+        var targetIndex = (checkOffset + targetOffset) / pageLength
+
+        if velocityValue > 0 {
+            targetIndex = ceil(targetIndex)
+        } else if velocityValue < 0 {
+            targetIndex = floor(targetIndex)
+        } else {
+            let (maxFloorIndex, lastInterval) = modf(lastPage)
+            if targetIndex > maxFloorIndex {
+                targetIndex = (targetIndex >= lastInterval / 2 + maxFloorIndex) ? lastPage : maxFloorIndex
+            } else {
+                targetIndex = round(targetIndex)
+            }
+        }
+
+        let offset: CGFloat
+
+        if targetIndex >= lastPage {
+            offset = maxOffset
+        } else if targetIndex <= firstPage {
+            offset = minOffset
+        } else {
+            offset = minOffset + pageLength * targetIndex
+        }
+
+        if scrollDirection == .horizontal {
+            targetContentOffset.pointee.x = offset
+        } else {
+            targetContentOffset.pointee.y = offset
+        }
     }
 
     /// Advances contentOffset by one page in the given direction.
